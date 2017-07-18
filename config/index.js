a415c7944f4a5fa6e3271dd6a147572cd222051e
Ryan Crosser <ryankcrosser@gmail.com>
2017-07-17 22:30:21 -0400

    try again

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'https://ryancrosser.github.io/ngv-prototype',
+    assetsPublicPath: 'https://ryancrosser.github.io/ngv-prototype/dist/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
